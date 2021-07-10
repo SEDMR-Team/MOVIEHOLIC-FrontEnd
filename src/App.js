26a904422b22737ae3b395ca53bdb8bf78b59004
Duniaalkilany <duniaalkilany908@gmail.com>
2021-07-10 18:42:57 +0300

    starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { withAuth0 } from "@auth0/auth0-react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ import Header from './components/Header.js';
+ import Footer from './components/Footer.js';
+import axios from 'axios';
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
