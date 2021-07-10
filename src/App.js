ce1c55a940661eabb4d1918029dc3f67042c7e06
Duniaalkilany <duniaalkilany908@gmail.com>
2021-07-10 20:12:58 +0300

    working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import axios from 'axios';
+import {BrowserRouter as Router , Switch, Route} from 'react-router-dom'
 // import { withAuth0 } from "@auth0/auth0-react";
 import Header from './components/Header.js';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Profile from './components/Profile';
-import {Router , Switch, Route} from 'react-router-dom'
 import './App.css'; 
 
 
@@ -17,7 +17,7 @@ export class App extends Component {
       <div>
 
         <Router>
-          <Header />
+          {/* <Header /> */}
          
           <Switch>
             <Route exact path="/">
@@ -29,7 +29,7 @@ export class App extends Component {
             </Route>
           </Switch>
          
-          <Footer />
+          {/* <Footer /> */}
 
         </Router>
       </div>
