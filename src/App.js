5e00173259c577a2f81f8798b634d1549585162d
sultan-elayan <sultan.m.elayan@gmail.com>
2021-07-10 19:56:39 +0300

    make the basic element

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { withAuth0 } from "@auth0/auth0-react";
-import './App.css';
- import Header from './components/Header.js';
- import Footer from './components/Footer.js';
-import axios from 'axios';
+// import axios from 'axios';
+// import { withAuth0 } from "@auth0/auth0-react";
+import Header from './components/Header.js';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Profile from './components/Profile';
+import {Router , Switch, Route} from 'react-router-dom'
+import './App.css'; 
+
+
+
 export class App extends Component {
   render() {
     return (
       <div>
-        
+
+        <Router>
+          <Header />
+         
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/profile">
+            <Profile />
+            </Route>
+          </Switch>
+         
+          <Footer />
+
+        </Router>
       </div>
     )
   }
---
+++ src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LoginPage extends Component {
+    render() {
+        return (
+            <div>
+                   <LoginButton/> 
+            </div>
+        )
+    }
+}
+
+export default LoginPage
