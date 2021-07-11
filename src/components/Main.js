22d8e09a0391221435f34704f3a9c6732f2c8b75
Duniaalkilany <duniaalkilany908@gmail.com>
2021-07-11 15:51:59 +0300

    searching

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,17 @@ import React from 'react';
 // import Cards from './Cards';
 import MoviesColumns from './MoviesColumns.js';
 // import { Container, CardColumns } from 'react-bootstrap';
+import SearchBar from './SearchBar';
 class Main extends React.Component {
   render() {
     return (
       <>
+       <SearchBar
+          handleOnChange={this.props.handleOnChange} 
+          handleSubmit={this.props.handleSubmit}
+          with_genres={this.props.with_genres}
+          primary_release_year={this.props.primary_release_year}
+        />
                <MoviesColumns movies={this.props.movies} />
       </>
     )
