16e0c609796ab029dac216712816b20a959dcba6
sultan-elayan <sultan.m.elayan@gmail.com>
2021-07-11 17:15:39 +0300

    adding some components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export class Home extends Component {
         return (
             <div>
                 <h1>hello this home page </h1>
+               
             </div>
         )
     }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Row } from 'react-bootstrap';
 // import Cards from './Cards';
 import MoviesColumns from './MoviesColumns.js';
+import SlideShow from './SlideShow.js';
 // import { Container, CardColumns } from 'react-bootstrap';
+
+
 class Main extends React.Component {
   render() {
     return (
       <>
                <MoviesColumns movies={this.props.movies} />
+               {/* <Pagination/> */}
+               <SlideShow/>
+              
       </>
     )
   }
